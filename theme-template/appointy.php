424690524ce52260e01bf995f8e8006a9c3b6f96
nemeshsingh <nemeshsingh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-20 11:57:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/appointy-appointment-scheduler/trunk@81345 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ theme-template/appointy.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: appointy.php
+*/
+?>
+<style type="text/css">
+<!--
+.fullwidth {
+	float: left;
+	padding: 0 0 20px 45px;
+	margin: 0px 0 0;
+	width: 650px;
+	}
+-->
+</style>
+<?php get_header(); ?>
+
+	<div id="content" class="fullwidth">
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div class="post" id="post-<?php the_ID(); ?>">
+		<h2><?php the_title(); ?></h2>
+			<div class="entry">
+				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+
+				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+
+			</div>
+		</div>
+		<?php endwhile; endif; ?>
+	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+	</div>
+
+<?php get_footer(); ?>
