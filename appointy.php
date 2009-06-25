9261cd44ca4012fa6f5749c03628cf9e89668e7b
nemeshsingh <nemeshsingh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-25 11:35:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/appointy-appointment-scheduler/trunk@129330 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- appointy.php
+++ appointy.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.appointy.com/wordpress/
 Description: This plugin shows your free time slot on your blog and allows you to book appointments with your clients 24x7x365. Very easy Ajax interface. Easy to setup and can be controlled completely from powerful admin area.
 Version: 1.1
 Author: Appointy, Inc	
-Author URI: www.appointy.com
+Author URI: http://www.appointy.com
 */
 
 //define('WP_DEBUG', true);
